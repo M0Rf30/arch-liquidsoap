3f4b8f8d5afb152dc683d30597e0163648c83221
M0Rf30 <morf3089@gmail.com>
2018-06-16 15:46:30 +0200

    trial

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,5 @@
 FROM m0rf30/arch-yay:latest
 LABEL authors="M0Rf30"
 COPY resources /resources
-
 RUN /resources/entrypoint.sh
-
-# Perform a clean
-RUN yay -Yc --noconfirm
-RUN pacman -Rscn yay --noconfirm
-RUN yes y | pacman -Scc --noconfirm
 USER liquidsoap
--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-set -x
-#su -c - user "yay -S ocaml-camomile ocaml-gd4o ocaml-lablgl ocaml-ocamlsdl ocaml-pcre ocaml-result ocaml-ssl ocaml-topkg ocaml-xmlm perl-xml-dom --noconfirm && yay -U /resources/liquidsoap-1.3.3-2-x86_64.pkg.tar --noconfirm"
-su -c - user "yay -S liquidsoap"
-su -c - user "yes y | yay -Scc"
+yay -S liquidsoap --noconfirm
+yes y | yay -Scc
